adc2d292eafc446b551073eabfbb2b69bf77acf0
francesJD <frances.jdong@gmail.com>
2021-12-10 14:32:55 -0800

    2rd change

--- src/main/java/com/example/imagemanagement/ImageController.java
+++ src/main/java/com/example/imagemanagement/ImageController.java
@@ -1,15 +1,22 @@
 package com.example.imagemanagement;
 
+import com.drew.metadata.exif.ExifIFD0Directory;
+import com.drew.metadata.exif.ExifSubIFDDescriptor;
+import com.drew.metadata.exif.ExifSubIFDDirectory;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Camera;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 
-import java.io.*;
+import java.io.File;
+import java.util.Date;
 
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.metadata.*;
 
 public class ImageController {
     @FXML
@@ -20,6 +27,24 @@ public class ImageController {
 
     @FXML
     private TextField widthTextField = new TextField();
+
+    @FXML
+    private TextField cameraTextField = new TextField();
+
+    @FXML
+    private TextField datetimeTextField = new TextField();
+
+    @FXML
+    public TextField exposureTextField = new TextField();
+
+    @FXML
+    public TextField fNumberTextField = new TextField();
+
+    @FXML
+    public TextField isoTextField = new TextField();;
+
+    @FXML
+    public TextField lensTextField = new TextField();;
 
     @FXML
     private TextField pathTextField = new TextField();
@@ -39,6 +64,34 @@ public class ImageController {
         tmp = fc.showOpenDialog(null);
 
         if (tmp != null) {
+            try {
+                Metadata metadata = ImageMetadataReader.readMetadata(tmp);
+                for (Directory directory : metadata.getDirectories()) {
+                    for (Tag tag : directory.getTags()) {
+                        System.out.println(tag);
+                    }
+                }
+                // obtain the Exif directory
+                ExifIFD0Directory ifd0Directory = metadata.getFirstDirectoryOfType(ExifIFD0Directory.class);
+                // query the tag's value
+                String camera = ifd0Directory.getDescription(ExifSubIFDDirectory.TAG_MODEL);
+                cameraTextField.setText(camera);
+                String datetime = ifd0Directory.getDescription(ExifSubIFDDirectory.TAG_DATETIME);
+                datetimeTextField.setText(datetime);
+                // obtain the Exif subdirectory
+                ExifSubIFDDirectory subIFDDirectory = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
+                String exposure = subIFDDirectory.getDescription(ExifSubIFDDirectory.TAG_EXPOSURE_TIME);
+                exposureTextField.setText(exposure);
+                String fNumber = subIFDDirectory.getDescription(ExifSubIFDDirectory.TAG_FNUMBER);
+                fNumberTextField.setText(fNumber);
+                String iso = subIFDDirectory.getDescription(ExifSubIFDDirectory.TAG_ISO_EQUIVALENT);
+                isoTextField.setText(iso);
+                String lens = subIFDDirectory.getDescription(ExifSubIFDDirectory.TAG_LENS_MODEL);
+                lensTextField.setText(lens);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
             Image img = new Image(tmp.toURI().toString());
             imageView.setImage(img);
             Double h = img.getHeight();
